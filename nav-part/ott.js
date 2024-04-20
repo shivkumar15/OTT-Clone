37de3a4513c80d93183a54b19dd314721385ebae
shivkumar15 <161976963+shivkumar15@users.noreply.github.com>
2024-04-20 11:44:48 +0530

    nav part

---
+++ nav-part/ott.js
@@ -0,0 +1,8 @@
+let navANDlogo_hover=document.querySelector(".navANDlogo");
+navANDlogo_hover.addEventListener("mouseover",function(){
+    navANDlogo_hover.classList.add("navANDlogo-hover");
+})
+
+navANDlogo_hover.addEventListener("mouseleave",function(){
+    navANDlogo_hover.classList.remove("navANDlogo-hover");
+})
